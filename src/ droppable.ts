5cec813d0cfec403ccc461ceceaa7f8e2376af32
Wayne Van Son <waynevanson@gmail.com>
2022-07-30 09:54:08 +1000

    feat(takeable)!: replace `DroppableLeft` with `Droppable` typeclass

--- src/ droppable.ts
+++ src/ droppable.ts
@@ -11,78 +11,74 @@ import {
 } from "fp-ts/HKT"
 import { Option } from "fp-ts/lib/Option"
 
-export interface DroppableLeft<F> {
+export interface Droppable<F> {
   readonly URI: F
-  readonly dropLeft: (count: number) => <A>(fa: HKT<F, A>) => HKT<F, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly drop: (count: number) => <A>(fa: HKT<F, A>) => HKT<F, A>
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: HKT<F, A>) => HKT<F, A>
 }
 
-export interface DroppableLeft1<F extends URIS> {
+export interface Droppable1<F extends URIS> {
   readonly URI: F
-  readonly dropLeft: (count: number) => <A>(fa: Kind<F, A>) => Kind<F, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly drop: (count: number) => <A>(fa: Kind<F, A>) => Kind<F, A>
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: Kind<F, A>) => Kind<F, A>
 }
 
-export interface DroppableLeft2<F extends URIS2> {
+export interface Droppable2<F extends URIS2> {
   readonly URI: F
-  readonly dropLeft: (
-    count: number
-  ) => <A, E>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly drop: (count: number) => <A, E>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <E>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
 }
 
-export interface DroppableLeft2C<F extends URIS2, E> {
+export interface Droppable2C<F extends URIS2, E> {
   readonly URI: F
-  readonly dropLeft: (
-    count: number
-  ) => <A>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly drop: (count: number) => <A>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: Kind2<F, E, A>) => Kind2<F, E, A>
 }
 
-export interface DroppableLeft3<F extends URIS3> {
+export interface Droppable3<F extends URIS3> {
   readonly URI: F
-  readonly dropLeft: (
+  readonly drop: (
     count: number
   ) => <R, E, A>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <R, E>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
 }
 
-export interface DroppableLeft3C<F extends URIS3, E> {
+export interface Droppable3C<F extends URIS3, E> {
   readonly URI: F
-  readonly dropLeft: (
+  readonly drop: (
     count: number
   ) => <R, A>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <R>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
 }
 
-export interface DroppableLeft4<F extends URIS4> {
+export interface Droppable4<F extends URIS4> {
   readonly URI: F
-  readonly dropLeft: (
+  readonly drop: (
     count: number
   ) => <S, R, E, A>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <S, R, E>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
 }
 
-export interface DroppableLeft4C<F extends URIS4, E> {
+export interface Droppable4C<F extends URIS4, E> {
   readonly URI: F
-  readonly dropLeft: (
+  readonly drop: (
     count: number
   ) => <S, R, A>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
-  readonly dropLeftWhileMap: <A, B>(
+  readonly dropWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <S, R>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
 }
---
+++ src/droppable-with-index.ts
@@ -0,0 +1,73 @@
+import {
+  HKT,
+  Kind,
+  Kind2,
+  Kind3,
+  Kind4,
+  URIS,
+  URIS2,
+  URIS3,
+  URIS4,
+} from "fp-ts/HKT"
+import { Option } from "fp-ts/lib/Option"
+import {
+  Droppable,
+  Droppable1,
+  Droppable2,
+  Droppable2C,
+  Droppable3,
+  Droppable3C,
+  Droppable4,
+  Droppable4C,
+} from "./ droppable"
+
+export interface DroppableWithIndex<F, I> extends Droppable<F> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => (fa: HKT<F, A1>) => HKT<F, A2>
+}
+
+export interface DroppableWithIndex1<F extends URIS, I> extends Droppable1<F> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => (fa: Kind<F, A1>) => Kind<F, A2>
+}
+
+export interface DroppableWithIndex2<F extends URIS2, I> extends Droppable2<F> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A, E>(fa: Kind2<F, E, A>) => Kind2<F, E, A2>
+}
+
+export interface DroppableWithIndex2C<F extends URIS2, I, E>
+  extends Droppable2C<F, E> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A>(fa: Kind2<F, E, A>) => Kind2<F, E, A2>
+}
+
+export interface DroppableWithIndex3<F extends URIS3, I> extends Droppable3<F> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A, R, E>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A2>
+}
+
+export interface DroppableWithIndex3C<F extends URIS3, E, I>
+  extends Droppable3C<F, E> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A, R>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A2>
+}
+
+export interface DroppableWithIndex4<F extends URIS4, I> extends Droppable4<F> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A, S, R, E>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A2>
+}
+
+export interface DroppableWithIndex4C<F extends URIS4, E, I>
+  extends Droppable4C<F, E> {
+  readonly dropWhileMapWithIndex: <A1, A2>(
+    f: (i: I, a: A1) => Option<A2>
+  ) => <A, S, R>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A2>
+}
