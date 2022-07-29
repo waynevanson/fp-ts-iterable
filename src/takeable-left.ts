6e901c2bfec07740697201584ba16a94c9aa3242
Wayne Van Son <waynevanson@gmail.com>
2022-07-30 08:18:43 +1000

    feat(takeable-left): add `take` fn to typeclass

--- src/takeable-left.ts
+++ src/takeable-left.ts
@@ -12,48 +12,76 @@ import {
 import { Option } from "fp-ts/lib/Option"
 
 export interface TakeableLeft<F> {
+  readonly URI: F
+  readonly takeLeft: (count: number) => <A>(fa: HKT<F, A>) => HKT<F, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: HKT<F, A>) => HKT<F, B>
 }
 
 export interface TakeableLeft1<F extends URIS> {
+  readonly URI: F
+  readonly takeLeft: (count: number) => <A>(fa: Kind<F, A>) => Kind<F, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: Kind<F, A>) => Kind<F, B>
 }
 
 export interface TakeableLeft2<F extends URIS2> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <A, E>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <E>(fa: Kind2<F, E, A>) => Kind2<F, E, B>
 }
 
 export interface TakeableLeft2C<F extends URIS2, E> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <A>(fa: Kind2<F, E, A>) => Kind2<F, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => (fa: Kind2<F, E, A>) => Kind2<F, E, B>
 }
 
 export interface TakeableLeft3<F extends URIS3> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <R, E, A>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <R, E>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, B>
 }
 
 export interface TakeableLeft3C<F extends URIS3, E> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <R, A>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <R>(fa: Kind3<F, R, E, A>) => Kind3<F, R, E, B>
 }
 
 export interface TakeableLeft4<F extends URIS4> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <S, R, E, A>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <S, R, E>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, B>
 }
 
 export interface TakeableLeft4C<F extends URIS4, E> {
+  readonly URI: F
+  readonly takeLeft: (
+    count: number
+  ) => <S, R, A>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, A>
   readonly takeLeftWhileMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <S, R>(fa: Kind4<F, S, R, E, A>) => Kind4<F, S, R, E, B>
